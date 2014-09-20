ead91b9a637297be80505e193290f4ebca51a2b2
Misho Krastev <misho.kr@gmail.com>
2014-09-19 23:59:27 -0700

    first version of the playbook, more testing needed

---
+++ ansible/setup-minimal-vm/files/devenv.sh
@@ -0,0 +1,18 @@
+# -- aliases ---------------------------------------------------
+
+alias l='ls -CF'
+alias la='ls -aCF'
+alias ll='ls -l'
+alias lla='ls -al'
+alias ltr='ls -ltr'
+
+# alias sv='${HOME}/bin/set_dyn_view.pl'
+
+alias d='docker'
+alias e='emacs -nw'
+alias g='git'
+alias vir='vi -R'
+
+# --------------------------------------------------------------
+# eof
+#
