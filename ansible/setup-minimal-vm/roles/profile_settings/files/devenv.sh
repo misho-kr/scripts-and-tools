78b77eca554be8c15c20929a7a1ce278071c84d3
Misho Krastev <misho.kr@gmail.com>
2015-07-03 15:53:50 -0700

    refactor setup-minimal-vm Ansible playbook, work-in-progress

--- ansible/setup-minimal-vm/roles/profile_settings/files/devenv.sh
+++ ansible/setup-minimal-vm/roles/profile_settings/files/devenv.sh
@@ -8,11 +8,14 @@ alias ltr='ls -ltr'
 
 # alias sv='${HOME}/bin/set_dyn_view.pl'
 
-[[ -x /usr/bin/emacs24-nox ]]	 && alias enox='emacs24-nox'
+[[ -x /usr/bin/emacs-nox ]]      && alias enox='emacs-nox'
+[[ -x /usr/bin/emacs24-nox ]]    && alias enox='emacs24-nox'
 [[ -x /usr/bin/emacs-24.3-nox ]] && alias enox='emacs-24.3-nox'
+
 alias e='enox'
 
 alias d='docker'
+alias dcp='docker-compose'
 alias g='git'
 alias vir='vi -R'
 
